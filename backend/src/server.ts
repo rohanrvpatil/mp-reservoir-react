832647bc9e1e3b318913d10caf1b9996cad07739
Rohan Patil <rohan.rv.patil@gmail.com>
2024-10-09 15:54:42 +0530

    PCR: minor changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ const PORT = 3000;
 
 app.use(
   cors({
-    origin: [""],
+    origin: ["https://mp-reservoir-react-frontend.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
