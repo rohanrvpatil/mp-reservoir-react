1f7e469370fc22c01eb59ebd097d90bad94d55ab
Rohan Patil <rohan.rv.patil@gmail.com>
2024-10-09 16:45:00 +0530

    PCR: minor changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
   axios.defaults.withCredentials = true;
 
   useEffect(() => {
-    const apiUrl = "https://www.mp-reservoir-react-backend.vercel.app";
+    const apiUrl = "https://mp-reservoir-react-backend.vercel.app";
 
     fetch(`${apiUrl}/reservoir-water-level`)
       .then((response) => response.text())
